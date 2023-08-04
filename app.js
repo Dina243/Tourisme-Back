a3654c380c6f63c08d33b4aafd838e5652de1dba
Omega243 <140796147+Dina243@users.noreply.github.com>
2023-08-04 22:50:34 +0300

    Update notification prod

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ const port = 3000;
 
 // Ajout pour notif 04/08/2023
 const admin = require('firebase-admin');
-const serviceAccount = require('D:/Dina/Cours_Master1_P10/ProjetFinal_Android_MrRojo_M1_P10/projet/clé_API/mada-tourisme7-firebase-adminsdk-mx1vz-d69a153297.json');
+//const serviceAccount = require('D:/Dina/Cours_Master1_P10/ProjetFinal_Android_MrRojo_M1_P10/projet/clé_API/mada-tourisme7-firebase-adminsdk-mx1vz-d69a153297.json');
 // Pour le déploiement le chemin d'accès spécifié ne fonctionnera pas donc
         // Définir une variable d'environnement temporaire dans cmd :
                 // SET FIREBASE_SERVICE_ACCOUNT_KEY_PATH=D:\Dina\Cours_Master1_P10\ProjetFinal_Android_MrRojo_M1_P10\projet\clé_API\mada-tourisme7-firebase-adminsdk-mx1vz-d69a153297.json
         // Définir une variable d'environnement permanente dans cmd :
                 // setx FIREBASE_SERVICE_ACCOUNT_KEY_PATH "D:\Dina\Cours_Master1_P10\ProjetFinal_Android_MrRojo_M1_P10\projet\clé_API\mada-tourisme7-firebase-adminsdk-mx1vz-d69a153297.json"
-    // const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
